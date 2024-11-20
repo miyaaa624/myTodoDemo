bf347597eab7f957fee53bc51d684f881e956ae4
miyaaa624 <a09837290595@gmail.com>
2024-11-20 14:54:13 +0800

    1120-1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //建立開發中的路徑
+  //base:process.env.NODE_ENV => 取得node環境中的變數
+  //如果目前的路徑=產品路徑,就執行倉庫的路徑
+  base:process.env.NODE_ENV === 'production' ?'/myTodoDemo/' :'/',
   plugins: [react()],
 })
